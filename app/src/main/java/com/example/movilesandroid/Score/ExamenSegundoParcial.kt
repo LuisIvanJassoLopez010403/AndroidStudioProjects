1049f8d9ea6e945af03f3ca3e9fd0d979a23c318
Luis Ivan Jasso Lopez <11995@ulsachihuahua.edu.mx>
2024-05-18 16:06:33 -0600

    Se hicieron cambios a ExamenSegundoParcial

--- app/src/main/java/com/example/movilesandroid/Score/ExamenSegundoParcial.kt
+++ app/src/main/java/com/example/movilesandroid/Score/ExamenSegundoParcial.kt
@@ -67,7 +67,7 @@ fun ExamenSegundoParcial(viewModel: SoccerScoreViewModel) {
                         Log.d("sumar", "local")
                         viewModel.incrementVisitScore(SoccerScoreVisitModel(visitScoreResult))
                     }
-            )
+            ) 
         }
     }
 }
