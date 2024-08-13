d53b5b83a16916655a44bc8e03076c87ce6ddd01
Luis Ivan Jasso Lopez <11995@ulsachihuahua.edu.mx>
2024-08-12 19:31:17 -0600

    Se agrega pantalla de navegacion principal

--- app/src/main/java/com/example/movilesandroid/MainActivity.kt
+++ app/src/main/java/com/example/movilesandroid/MainActivity.kt
@@ -9,7 +9,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.movilesandroid.Screens.Restaurants.Views.Nav
+import com.example.movilesandroid.CookBook.MainScreen
 import com.example.movilesandroid.ui.theme.MovilesAndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,8 +18,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             MovilesAndroidTheme {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Nav()
-                    //OpenURL()
+                    MainScreen()
+                    //Nav()
                 }
             }
         }
@@ -30,7 +30,6 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingPreview() {
     MovilesAndroidTheme {
-        Nav()
-        //OpenURL()
+        MainScreen()
     }
 }
