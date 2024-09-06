bbbeaf4f625b326cea1ed07039983d99befce167
Luis Ivan Jasso Lopez <11995@ulsachihuahua.edu.mx>
2024-09-06 15:52:06 -0600

    Se hacen modificaciones esteticas a la vista

--- app/src/main/java/com/example/movilesandroid/firstPartialExam/FirstPartialExamView.kt
+++ app/src/main/java/com/example/movilesandroid/firstPartialExam/FirstPartialExamView.kt
@@ -9,16 +9,21 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -33,10 +38,11 @@ import com.example.movilesandroid.R
 
 @Composable
 fun FirstPartialExamView(viewModel: FirstPartialExamViewModel,navController: NavController) {
-    val currentProduction by viewModel.getCurrentProduction().observeAsState(initial = 0)
+
     var totalProduction by remember {
         mutableStateOf("")
     }
+    val currentProduction by viewModel.getCurrentProduction().observeAsState(initial = 0)
     val percentage by viewModel.getPercentage().observeAsState(0.0)
 
     val productionUnits = viewModel.calculateCurrentProductionUnits()
@@ -80,14 +86,18 @@ fun FirstPartialExamView(viewModel: FirstPartialExamViewModel,navController: Nav
             TextField(
                 value = totalProduction,
                 onValueChange = {totalProduction = it},
+                colors = TextFieldDefaults.colors(
+                    unfocusedContainerColor = Color.Transparent,
+                    focusedContainerColor = Color.Transparent,
+                ),
                 modifier = Modifier
-                    .width(100.dp)
+                    .width(150.dp)
             )
             Image(
                 painter = painterResource(id = R.drawable.apple),
                 contentDescription = "",
                 modifier = Modifier
-                    .size(30.dp)
+                    .size(50.dp)
                     .clickable {
                         Toast.makeText(
                             context,
@@ -110,14 +120,18 @@ fun FirstPartialExamView(viewModel: FirstPartialExamViewModel,navController: Nav
             TextField(
                 value = "$currentProduction",
                 onValueChange = {},
+                colors = TextFieldDefaults.colors(
+                    unfocusedContainerColor = Color.Transparent,
+                    focusedContainerColor = Color.Transparent,
+                ),
                 modifier = Modifier
-                    .width(100.dp)
+                    .width(150.dp)
             )
             Image(
                 painter = painterResource(id = R.drawable.apple),
                 contentDescription = "",
                 modifier = Modifier
-                    .size(30.dp)
+                    .size(50.dp)
                     .clickable {
                         Toast.makeText(
                             context,
@@ -133,27 +147,73 @@ fun FirstPartialExamView(viewModel: FirstPartialExamViewModel,navController: Nav
             modifier = Modifier
                 .padding(vertical = 16.dp)
         ) {
-            Button(onClick = { viewModel.sumProduction(5) }) {
+            Button(
+                onClick = { viewModel.sumProduction(5) },
+                modifier = Modifier
+                    .width(75.dp)
+                    .height(60.dp),
+                shape = RoundedCornerShape(5.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Blue)
+            ) {
                 Text(text = "+5")
             }
-            Button(onClick = { viewModel.sumProduction(15) }) {
+            Button(
+                onClick = { viewModel.sumProduction(15) },
+                modifier = Modifier
+                    .width(75.dp)
+                    .height(60.dp),
+                shape = RoundedCornerShape(5.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Blue)
+                ) {
                 Text(text = "+15")
             }
-            Button(onClick = { viewModel.sumProduction(30) }) {
+            Button(onClick = { viewModel.sumProduction(30) },
+                modifier = Modifier
+                    .width(75.dp)
+                    .height(60.dp),
+                shape = RoundedCornerShape(5.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Blue)
+                ) {
                 Text(text = "+30")
             }
-            Button(onClick = { viewModel.sumProduction(50) }) {
+            Button(onClick = { viewModel.sumProduction(50) },
+                modifier = Modifier
+                    .width(75.dp)
+                    .height(60.dp),
+                shape = RoundedCornerShape(5.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Blue)
+            ) {
                 Text(text = "+50")
             }
         }
-        Row {
-            Text(text = "Porcentaje")
-            TextField(value = "$percentage", onValueChange = {})
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(10.dp),
+            modifier = Modifier
+                .padding(vertical = 16.dp)
+        ) {
+            Text(
+                text = "Porcentaje",
+                color = Color.LightGray
+            )
+            TextField(
+                value = "$percentage",
+                onValueChange = {},
+                colors = TextFieldDefaults.colors(
+                    unfocusedContainerColor = Color.Transparent,
+                    focusedContainerColor = Color.Transparent,
+                ),
+                modifier = Modifier
+                    .width(250.dp)
+            )
         }
-        Button(onClick = {
-            viewModel.calculatePercentage(totalProduction.toInt())
-
-        }) {
+        Button(
+            onClick = { viewModel.calculatePercentage(totalProduction.toInt()) },
+            modifier = Modifier
+                .width(150.dp),
+            shape = RoundedCornerShape(5.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Blue)
+        ) {
             Text(text = "Calcular")
         }
     }
